8c9843bf8d8b6aaed8e7ba8f3bfea91e9629e60b
zulfahmidev <developer.zulfahmi@gmail.com>
2023-07-07 17:23:40 +0700

    memperbaiki footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
   <footer id="footer">
-    <div class="copy">Copyright &copy;2021, Developed by <a href="https://zulfahmidev.github.io" class="link">zulfahmidev</a>.</div>
+    <div class="copy">Copyright &copy;2021, Developed by <a href="https://zulfahmidev.com" class="link">zulfahmidev</a>.</div>
   </footer>
 </template>
