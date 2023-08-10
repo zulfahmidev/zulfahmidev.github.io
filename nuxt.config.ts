504da1ece117ae351c6f9b5edd671a6d2bd68280
zulfahmidev <developer.zulfahmi@gmail.com>
2023-08-10 10:23:56 +0700

    add meta data

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,27 @@ export default defineNuxtConfig({
   app: {
     head: {
       charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1'
+      viewport: 'width=device-width, initial-scale=1',
+      meta: [
+        {
+          name: 'description', content: 'Hai, saya Zulfahmi, seorang pengembang yang berfokus pada pengembangan Web dan Aplikasi Selular yang berpengalaman selama empat tahun. Sudah tersertifikasi nasional oleh Lembaga Sertifikasi Profesi Komputer dalam bidang Programming and Software Development pada tahun 2020. Kepuasan pelanggan adalah prioritas saya, dan perlu di ingat, bahwa aplikasi dari saya sepenuhnya dibuat dengan sepenuh hati dan tidak asal jadi.'
+        },
+        {
+          name: 'application-name', content: 'Zulfahmidev'
+        },
+        {
+          name: 'creator', content: 'Zulfahmi'
+        },
+        {
+          name: 'author', content: 'Zulfahmi'
+        },
+        {
+          name: 'keywords', content: 'zulfahmi, zulfahmidev, indonesia, aceh, lhokseumawe, mobile, web, website, mobile app, android, ios, react native, flutter, swift, web developer, web enginer, software enginering, pengembang web, mobile enginering, mobile developer, pengembang aplikasi mobile, pengembang, perangkat lunak, professional, wordskill, lomba kopetensi siswa, sertifikasi nasional, lembaga sertifikasi profesi komputer, komputer, computer, programming, programmer, bangkit, acedmy'
+        },
+        {
+          name: 'title', content: 'Zulfahmidev'
+        }
+      ]
     }
   }
 })
