b7c1020c17ec6196b03729a1f442a084e9ba85f6
zulfahmidev <developer.zulfahmi@gmail.com>
2025-09-28 18:17:33 +0700

    update about

--- components/ui/home/about-me.tsx
+++ components/ui/home/about-me.tsx
@@ -1,5 +1,4 @@
 import { FaArrowRight } from "react-icons/fa";
-import { TextAnimate } from "../text-animate";
 
 export default function AboutMe() {
     return (
@@ -7,13 +6,12 @@ export default function AboutMe() {
             <div className="container mx-auto py-16 px-5 lg:px-0" id="about">
                 <h2 className="text-center text-xl text-primary">About Me</h2>
 
-                <TextAnimate animation="blurIn" as="p" delay={0.2} className="text-3xl lg:text-5xl text-center my-8">I am Zulfahmi, a Software Engineer with over 5 years of experience in creating high-quality and impactful digital experiences. I have a strong focus on transforming complex problems into elegant and user-friendly interfaces.</TextAnimate>
+                <p className="text-3xl lg:text-5xl text-center my-8">I am Zulfahmi, a Software Engineer with over 5 years of experience in creating high-quality and impactful digital experiences. I have a strong focus on transforming complex problems into elegant and user-friendly interfaces.</p>
                 <a href="" className="py-2 px-4 border-2 border-foreground rounded-full flex items-center gap-3 font-semibold w-fit mx-auto">
                     <span>See My Profile</span>
                     <FaArrowRight />
                 </a>
             </div>
-            {/* <TechStack /> */}
         </section>
     )
 }
