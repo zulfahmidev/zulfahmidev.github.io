e1133a4a4b601c17051402e7cab1d038d4d0db8b
zulfahmidev <developer.zulfahmi@gmail.com>
2025-09-28 18:36:39 +0700

    update logo navbar

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -31,7 +31,7 @@ export default function Navbar() {
                 transition={{ duration: 0.4, ease: "easeInOut" }}
                 className={`mx-auto h-16 flex items-center justify-between gap-8 bg-background/30 min-w-[90%] lg:min-w-fit px-5 lg:px-0 ${isScrolled ? 'rounded-full lg:px-10 backdrop-blur-md border border-foreground/10' : 'container'}`}
             >
-                <div className="h-8 w-8 relative">
+                <div className="h-8 aspect-square relative">
                     <Image
                         className="h-full dark:hidden inline-block"
                         src="/assets/logo-black.png"
