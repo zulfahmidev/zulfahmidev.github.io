5db79a40bbf59c481ec8f5cf557fac95c5e59ec7
zulfahmidev <developer.zulfahmi@gmail.com>
2025-09-29 07:42:54 +0700

    update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import Footer from "@/components/layout/footer";
 // import { ProgressiveBlur } from "@/components/ui/progressive-blur";
 import { Suspense } from "react";
 import { Analytics } from "@vercel/analytics/next"
+import { ProgressiveBlur } from "@/components/ui/progressive-blur";
 
 const suseMono = SUSE({
   variable: "--font-suse-mono",
@@ -116,7 +117,7 @@ export default function RootLayout({
             {children}
           </Suspense>
         </main>
-        {/* <ProgressiveBlur height="4rem" position="bottom" /> */}
+        <ProgressiveBlur height="6rem" position="bottom" />
         <Footer />
         <Analytics />
       </body>
